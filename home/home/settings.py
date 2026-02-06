53cb025f7d8743cd7f72d99ca7bc8d1653d211aa
Lizromans <lizethserna09@gmail.com>
2026-02-06 09:11:13 -0500

    configuración de proyecto

--- home/home/settings.py
+++ home/home/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'home.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],  # Asegúrate de que esta línea esté presente
+        'DIRS': [],  # Asegúrate de que esta línea esté presente
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
